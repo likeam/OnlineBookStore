b9b750f1f93db3cc833f3f185e3a424980babc2b
AbdulRehman <likeam99@gmail.com>
2022-05-16 14:41:11 +0500

    Building the Form Section

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
+
+//SEARCH FORM 
 let searchForm = document.querySelector('.search-form');
 document.querySelector('#search-btn').onclick = () =>{
     searchForm.classList.toggle('active');
 }
+
+//LOGIN FORM
+    let loginForm = document.querySelector('.login-form-container');
+    document.querySelector('#login-btn').onclick = () =>{
+        loginForm.classList.toggle('ative');
+    }
+    document.querySelector('#close-login-btn').onclick = () =>{
+        loginForm.classList.remove('.active');
+    }
