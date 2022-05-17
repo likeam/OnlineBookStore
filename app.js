92af4d6ae448e2298ea811333e1e808c25c68d94
AbdulRehman <likeam99@gmail.com>
2022-05-17 12:38:42 +0500

    Buiding the Footer Section

--- app.js
+++ app.js
@@ -25,3 +25,22 @@ document.querySelector('#search-btn').onclick = () =>{
             clickable: true,
         },
     });
+    indow.onload = () =>{
+
+        if(window.scrollY > 80){
+            document.querySelector('.header .header-2').classList.add('active');
+        }else{
+            document.querySelector('.header .header-2').classList.remove('active');
+        }
+        
+        fadeOut();
+        
+    }
+
+        function loader(){
+            document.querySelector('.loader-container').classList.add('active');
+        }
+        
+        function fadeOut(){
+        setTimeout(loader, 4000);
+    }
